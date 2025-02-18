5c2e6bcbb0beda7c664ee7bd0a5501cbe6843804
Felipe_andres <felipesabate98@gmail.com>
2025-02-18 16:28:53 -0300

    añadimos shopify y mejoramos rutas de prefect

--- dbt_project/models/mart/mart_inventario_venta.sql
+++ dbt_project/models/mart/mart_inventario_venta.sql
@@ -36,40 +36,10 @@ ventas AS (
         variant_code,
         variant_id,
         fecha_documento,
-        detail_quantity
+        SUM(detail_quantity) as unidades_vendidas
     FROM {{ ref('core_document_explode') }}
-    WHERE fecha_documento IS NOT NULL -- Aseguramos que no haya valores nulos
-),
-ventas_filtradas AS (
-    SELECT 
-        variant_code,
-        variant_id,
-        DATE_TRUNC(fecha_documento, WEEK(MONDAY)) AS semana, -- Agrupamos por semanas comenzando los lunes
-        SUM(detail_quantity) AS total_quantity,
-        COUNT(DISTINCT fecha_documento) AS dias_venta
-    FROM ventas
-    WHERE fecha_documento < CURRENT_DATE -- Excluimos la semana actual
-    GROUP BY variant_code, variant_id, semana -- Agregamos variant_code al GROUP BY
-),
-ventas_rankeadas AS (
-    SELECT 
-        variant_id,
-        variant_code,
-        semana,
-        total_quantity,
-        dias_venta,
-        ROW_NUMBER() OVER (PARTITION BY variant_id ORDER BY semana DESC) AS semana_num -- Numeramos semanas por cada variant_id
-    FROM ventas_filtradas
-),
-venta_final AS (
-    SELECT 
-        variant_id,
-        variant_code, -- Agregamos variant_code al SELECT
-        AVG(CASE WHEN semana_num BETWEEN 2 AND 5 THEN total_quantity / dias_venta END) AS avg_cuatro_semanas,
-        AVG(CASE WHEN semana_num BETWEEN 2 AND 3 THEN total_quantity / dias_venta END) AS avg_dos_semanas,
-        AVG(CASE WHEN semana_num = 2 THEN total_quantity / dias_venta END) AS avg_ultima_semana
-    FROM ventas_rankeadas
-    GROUP BY variant_id, variant_code -- Agregamos variant_code al GROUP BY
+    group by all
+ 
 )
 
 SELECT 
@@ -87,15 +57,13 @@ END AS orden_tallas,
     mg.variante_code_desc,
     mg.grupo_producto,
     mg.categoria,
-    vf.avg_cuatro_semanas,
-    vf.avg_dos_semanas,
-    vf.avg_ultima_semana,
-    s.stock_actual,
+    v.unidades_vendidas,
+    s.stock_actual
     
 FROM stock_actual s
 LEFT JOIN maestra_productos m 
     ON s.variant_code = m.sku
 LEFT JOIN raw_maestra_producto2 mg 
     ON LEFT(CAST(s.variant_code AS STRING), 4) = mg.variante_code_desc
-LEFT JOIN venta_final vf
-    ON s.variant_id = CAST(vf.variant_id AS STRING)
+LEFT JOIN ventas v
+    ON s.variant_id = CAST(v.variant_id AS STRING)
