3fc7ee430cfeea4aaf7a566282556dc36ce99fa2
Felipe_andres <felipesabate98@gmail.com>
2025-03-13 18:27:06 -0300

    cambio calculo ads Activos

--- dbt_project/models/mart/mart_meta_ads.sql
+++ dbt_project/models/mart/mart_meta_ads.sql
@@ -20,12 +20,16 @@ creative AS (
 
 
 ),
+
+-- Agregar filtro para poder contar ads
+
 adset_number AS (
-  SELECT 
+   SELECT 
     COUNT(DISTINCT ad_id) AS cantidad_ads,
-    adset_id
+    adset_id,
+    date_start
   FROM meta_ads
-  WHERE ad_is_active = 'Active'
+  WHERE spend > 0
   GROUP BY ALL 
 )
 
@@ -33,8 +37,10 @@ adset_number AS (
 SELECT 
 m.*,
 c.*,
-ad.cantidad_ads
+ad.cantidad_ads,
+(CASE WHEN CAST(m.adset_daily_budget AS FLOAT64) > 1 THEN CAST(m.adset_daily_budget AS FLOAT64) ELSE  CAST(m.campaign_daily_budget AS FLOAT64) END) / ad.cantidad_ads AS distributed_budget
+
 FROM meta_ads m 
 LEFT JOIN creative c  ON m.ad_id = c.ad_id_c
 LEFT JOIN adset_number ad
-  ON m.adset_id = ad.adset_id
+  ON m.adset_id = ad.adset_id AND m.date_start = ad.date_start
